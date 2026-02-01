a0bfa220bd73299ee8ac978b36395033a4c645aa
Matteo Granchelli <yozza@Mac.station>
2026-02-01 22:18:54 +0100

    try2q

--- lib/lib.ts
+++ lib/lib.ts
@@ -5,10 +5,10 @@ const sql = neon();
 
 export async function getPostById() {
     try {
-        const [post] = await sql`SELECT * FROM users`;
+        const post = await sql`SELECT * FROM users`;
         return post; // undefined se non esiste
     } catch (err) {
         console.error("Errore nel DB:", err);
-        return null;
+        return [];
     }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { getPostById } from "../../lib/lib";
+type Post = {
+  id: number;
+  name: string;
+};
 
 export default async function Home() {
-  const post = await getPostById(); // esempio: ID = 1
+  const posts: Post[] = (await getPostById()) as Post[];
 
-  if (!post) {
+  if (!posts || posts.length === 0) {
     return <div>Post non trovato</div>;
   }
-  const data = post.map((e: { id: number; name: string }) => (
+
+  const data = posts.map((e: Post) => (
     <div key={e.id}>
       <p>{e.id}</p>
       <p>{e.name}</p>
     </div>
   ));
+
   return (
     <div>
       <p>qui</p>
