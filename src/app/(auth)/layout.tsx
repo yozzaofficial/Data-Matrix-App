00cb1ef17728bbef14deaa8cff65ae327ada1cf3
Matteo Granchelli <yozza@Mac.station>
2026-02-03 21:31:37 +0100

    removed navbar in login page

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+export default function AuthLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
