c727f3da2d15fb8eb313461b11857410bff98093
Matteo Granchelli <yozza@Mac.station>
2026-02-14 14:35:54 +0100

    autologin fix final 4

---
+++ src/app/done/page.tsx
@@ -0,0 +1,8 @@
+export default function DonePage() {
+    return (
+        <main style={{ padding: "16px" }}>
+            <h3>Done</h3>
+            <p>Questa è la pagina Done.</p>
+        </main>
+    );
+}
