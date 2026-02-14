90bc00545655fa72aeb194b054a41d16a5561f33
Matteo Granchelli <yozza@Mac.station>
2026-02-14 19:40:00 +0100

    .

--- src/app/todo/layout.tsx
+++ src/app/todo/layout.tsx
@@ -3,22 +3,12 @@ import { Suspense } from "react";
 import "./../css/todo.css";
 import ToDoNav from "../components/todoComponents/TodoNav";
 import AutoLoginClient from "./AutoLoginClient";
-import { requireUser } from "../../../lib/auth";
 
-export default async function ToDoLayout({
+export default function ToDoLayout({
     children,
-    searchParams,
 }: {
     children: React.ReactNode;
-    searchParams?: { user?: string };
 }) {
-    // If there's no `user` query param, perform server-side auth check so
-    // unauthenticated visitors are redirected on the server (no flash).
-    const userParam = searchParams?.user;
-    if (!userParam) {
-        await requireUser("todo");
-    }
-
     return (
         <Suspense fallback={<div>Caricamento...</div>}>
             <AutoLoginClient />
