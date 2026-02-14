20f41892260e832d37ac72e085ad206f3270383c
Matteo Granchelli <yozza@Mac.station>
2026-02-14 11:55:27 +0100

    starting css in tablet mode

--- src/app/todo/layout.tsx
+++ src/app/todo/layout.tsx
@@ -1,43 +1,34 @@
-import { requireUser } from "../../../lib/auth"
+import { requireUser } from "../../../lib/auth";
 import { Suspense } from "react";
-import getRank from "../components/getRank";
-import Link from "next/link";
-import "./../css/todo.css"
+import "./../css/todo.css";
 import ToDoNav from "../components/todoComponents/TodoNav";
 
-type ToDoProps = Readonly<{
+async function ToDoLayout({
+    children,
+}: {
     children: React.ReactNode;
-    searchParams: {
-        user?: string;
-    };
-}>;
+}) {
+    const user = await requireUser("todo");
 
-async function ToDoLayout({ searchParams, children }: ToDoProps) {
-    const item = searchParams?.user || "nessun user";
-    const pathLocation = `todo?user=${item}`
-    const user = await requireUser(pathLocation);
-
-    const userRank = getRank()
-
-    return <>
-        <header className="todoHeader">
-            <h2 className="todoTitle">To Do List</h2>
-            <ToDoNav></ToDoNav>
-        </header>
-        {children}
-    </>
+    return (
+        <>
+            <header className="todoHeader">
+                <h2 className="todoTitle">To Do List</h2>
+                <ToDoNav />
+            </header>
+            {children}
+        </>
+    );
 }
 
 export default function ToDoList({
     children,
-    searchParams,
 }: {
     children?: React.ReactNode;
-    searchParams: { user?: string };
 }) {
     return (
         <Suspense fallback={<div>Caricamento...</div>}>
-            <ToDoLayout searchParams={searchParams}>{children}</ToDoLayout>
+            <ToDoLayout>{children}</ToDoLayout>
         </Suspense>
     );
 }
