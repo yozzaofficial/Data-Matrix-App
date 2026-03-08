efe693a28cf580a2a1843a8aba210f5f50d10832
Matteo Granchelli <yozza@Mac.station>
2026-03-08 09:52:45 +0100

    Real fix

--- src/app/components/NavButtons.tsx
+++ src/app/components/NavButtons.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import Image, { StaticImageData } from "next/image";
-import { useSearchParams, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 type Props = {
     path: string;
@@ -12,23 +12,10 @@ type Props = {
 };
 
 export default function NavButtons({ path, iconClicked, icon, name }: Props) {
+
     const pathname = usePathname();
-    const searchParams = useSearchParams();
 
-    // Controllo se il pulsante è attivo
-    const isActive = (() => {
-        if (path.includes("?")) {
-            const [basePath, query] = path.split("?");
-            const params = new URLSearchParams(query);
-
-            return pathname === basePath &&
-                Array.from(params.entries()).every(([key, value]) =>
-                    searchParams.get(key) === value
-                );
-        }
-
-        return pathname === path;
-    })();
+    const isActive = pathname === path.split("?")[0];
 
     return (
         <div className={`navButton ${isActive ? "active" : ""}`}>
